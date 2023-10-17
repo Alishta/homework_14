6de03022538822f0f21d83ff9f53375df78ad3dd
Alishta <arnery1234@gmail.com>
2023-10-17 14:38:50 +0300

    add files

---
+++ main.js
@@ -0,0 +1,4 @@
+let firstNumb = +prompt('enter a number');
+let secondNumb = +prompt('enter a number');
+let thirdNumb = +prompt('enter a number');
+alert (`(${firstNumb} + ${secondNumb} + ${thirdNumb}) / 3 = ${(firstNumb + secondNumb + thirdNumb)/3 }`);
